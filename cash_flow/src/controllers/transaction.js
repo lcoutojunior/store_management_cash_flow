b459c985cfa303b5c16118194c1a92c0770de8e3
Luiz Meirelles <lcoutojunior@gmail.com>
2023-05-21 21:12:46 -0300

    cash_flow container.

--- cash_flow/src/controllers/transaction.js
+++ cash_flow/src/controllers/transaction.js
@@ -1,15 +1,23 @@
 import express from "express";
 const router = express.Router();
 
-// Home page route
-router.get("/transactions", function (req, res) {
-  res.send("Transactions");
+const transactions = [];
+
+router.post("/transaction", function (req, res) {
+  const transaction = req.body;
+
+  if(transaction.type !== "credit" && transaction.type !== "debit"){
+    res.status(404).send("Invalid transaction.");  
+    return;
+  }
+
+  transactions.push(transaction);
+  console.log("POSTED: ", req.body);
+  res.status(201).send("Transaction Posted: " + JSON.stringify(req.body));
 });
 
-// About page route
-router.post("/transaction", function (req, res) {
-    console.log(req.body);
-    res.status(201).send("Transaction Posted");
+router.get("/transactions", function (req, res) {
+  res.status(200).send(transactions);
 });
 
 const controllers = router;
